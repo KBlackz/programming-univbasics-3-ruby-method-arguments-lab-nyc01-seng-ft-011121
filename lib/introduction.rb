dc11b11b3d141cfea52bc87b92db44d64797870c
Karris  Black <karris.b@gmail.com>
2020-12-23 23:44:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name)
-  puts "Hi, my name is #{josh}."
+def introduction(name = josh)
+  puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
+def introduction_with_language(name = "Josh", language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
